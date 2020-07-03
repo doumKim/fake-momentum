ddc9d91aad4de22908d6f3ce80d396bf627be9ba
doumKim <eha0129@naver.com>
2020-07-03 23:24:02 +0900

    Complete: make js-clock

---
+++ clock.js
@@ -0,0 +1,28 @@
+const clockContainer = document.querySelector('.js-clock');
+const clockTitle = clockContainer.querySelector('h1.clock-title');
+
+const getTime = () => {
+	const nowDate = new Date();
+	const hours = nowDate.getHours();
+	const minutes = nowDate.getMinutes();
+	const seconds = nowDate.getSeconds();
+	return { minutes, hours, seconds };
+};
+
+const makeClockTitle = () => {
+	const fetchToClockTitle = () => {
+		const timeInfo = getTime();
+		const { minutes, hours, seconds } = timeInfo;
+		clockTitle.textContent = `${hours < 10 ? `0${hours}` : `${hours}`}:${
+			minutes < 10 ? `0${minutes}` : `${minutes}`
+		}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`;
+	};
+	fetchToClockTitle();
+	const autoClockMove = setInterval(fetchToClockTitle, 1000);
+};
+
+const init = () => {
+	makeClockTitle();
+};
+
+init();
